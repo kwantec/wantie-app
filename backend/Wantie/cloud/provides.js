a2d30d7ed915d2ca86b4fdd4b64e9f4cd54c2111
Alfredo Repetto <alfredorepetto@Alfredos-MacBook-Pro.local>
2015-05-02 20:25:37 -0400

    Updated Provides

--- backend/Wantie/cloud/provides.js
+++ backend/Wantie/cloud/provides.js
@@ -1,32 +1,57 @@
+exports.providesMatch = function(request, response) {
+	
+	var theCity = request.params.city;
+	var theCategory = request.params.category;
+
+	var Want = Parse.Object.extend("Want");
+	var query = new Parse.Query(Want);
+
+	//query.equalTo("user")
+	
+	query.equalTo( "city",  theCity);
+	query.equalTo( "category",  theCategory);
+	query.descending("updatedAt");
+
+
+	//query.equalTo("user", user);
+	query.find({
+		success: function(matchesProvides) {
+			
+			response.success( matchesProvides )
+			
+		}
+	});
+	
+};
 
 
 exports.provides = function(request, response) {
-	var providesList = [
-		{
-			screenName: 'Daniel',
-			category: 'Plumbing',
-			city: 'Miami',
-			state: 'Florida',
-			country: 'USA',
-			description: '',
-			phone: '999-222-3333',
-			email: 'daniel@kwantec.com',
-			distance: 20,
-			unit: 'mi'
-		},
-		{
-			screenName: 'Bart',
-			category: 'Electrician',
-			city: 'Miami',
-			state: 'Florida',
-			country: 'USA',
-			description: '',
-			phone: '111-123-2333',
-			email: 'alfredo@kwantec.com',
-			distance: 20,
-			unit: 'mi'
-		},
-	];
   
-   response.success(providesList);
+	var currentUser = Parse.User.current();
+	if (currentUser) {
+		
+		if (currentUser.authenticated())
+		{
+			var Provide = Parse.Object.extend("Provide");
+			var query = new Parse.Query(Provide);
+			query.equalTo("myuser", currentUser );
+			query.find({
+			      success: function(usersProvides) {
+			        // usersProvides contains all of the wants by the current user.
+					  //console.log(userProvides);
+					  response.success(usersProvides);
+			      }
+			    });
+			
+		}else{
+			
+			response.error('User Not authenticated');
+		}
+		
+	    
+	} else {
+	    // show the signup or login page
+		response.error('No user or Not authenticated');
+	}
+   
 };
