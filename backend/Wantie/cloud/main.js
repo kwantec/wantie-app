6cff514a98d6578b400e2a8291efedf98e3ac2a9
Daniel Castaneda <daniel@kwantec.com>
2015-05-03 01:21:26 -0400

    Implemented matches

--- backend/Wantie/cloud/main.js
+++ backend/Wantie/cloud/main.js
@@ -9,8 +9,11 @@ var categoriessApi = require('cloud/categories.js');
 Parse.Cloud.define("wants_put", wantsApi.wants_put);
 Parse.Cloud.define("wants", wantsApi.wants);
 Parse.Cloud.define("wantsMatch", wantsApi.wantsMatch);
+Parse.Cloud.define("wantsnew", wantsApi.wantsnew);
+
 
 Parse.Cloud.define("provides", providesApi.provides );
+Parse.Cloud.define("providesMatch", providesApi.providesMatch );
 
 Parse.Cloud.define("settings", settingsApi.settings );
 
--- backend/Wantie/cloud/wants.js
+++ backend/Wantie/cloud/wants.js
@@ -11,7 +11,6 @@ exports.wantsMatch = function(request, response) {
 	var Provide = Parse.Object.extend("Provide");
 	var query = new Parse.Query(Provide);
 
-	//query.equalTo("user")
 	
 	query.equalTo( "city",  theCity);
 	query.equalTo( "category",  theCategory);
@@ -21,6 +20,9 @@ exports.wantsMatch = function(request, response) {
 	//query.equalTo("user", user);
 	query.find({
 		success: function(matchesWants) {
+			
+			console.log("MATCHES:" +  matchesWants.length);
+			console.log(matchesWants);
 			
 			response.success( matchesWants  )
 			
@@ -81,6 +83,178 @@ exports.wants_put = function(request, response) {
    
 };
 
+exports.wantsnew = function(request, response) {
+  
+			var Want = Parse.Object.extend("Want");
+			var query = new Parse.Query(Want);
+			//query.equalTo("myuser", currentUser );
+			
+
+			
+			query.find({
+			      success: function(usersWants) {
+			          // usersWants contains all of the wants by the current user.
+					  //console.log(userWants);
+					  
+					  var retUsers = [];
+					  var promise = new Parse.Promise();
+					  
+					  var resolved = false;
+					  
+					  promise.then(
+						  function(){
+							  console.log("EXECUTED PROMISE: ");
+						  	  response.success(retUsers);
+						  }
+					  );
+					  
+					  var tot = usersWants.length;
+					  var count = 1;
+					  
+					  for (var i = 0; i < usersWants.length; i++) { 
+						  var object = usersWants[i];
+						  var myjson = object.toJSON();
+						  retUsers.push(myjson);
+					  }
+					  
+					  for (var i = 0; i < usersWants.length; i++) { 
+						  
+						  console.log('PROCESSING: ' + i);
+						  console.log(object);
+						  
+						  var Provide = Parse.Object.extend("Provide");
+
+			  			  var q2 = new Parse.Query(Provide);
+						  q2.equalTo("city", retUsers[i].city );
+						  q2.equalTo("category", retUsers[i].category );
+						  q2.find({
+							  success: function(matchingProvides) {
+								  
+								  console.log("count VALUE (success): " + count);
+								  
+								  var cur =  retUsers[count-1];
+								  console.log("RetUsers:");
+								  console.log(retUsers);
+								  
+								  // usersWants contains all of the wants by the current user.
+								  //console.log(userWants);
+								  console.log('Found ' + matchingProvides.length + ' matches for want ' + retUsers[count-1].category);
+								  retUsers[count-1].matches = matchingProvides.length;
+								  
+								  count++;
+								  if ((count > tot) && (!resolved))
+								  {
+									  promise.resolve("Completed promise- called from success");
+									  resolved = true;
+								  }
+							  },
+							  error: function(error){
+								  
+								  retUsers[count-1].matches = 0;
+								  console.log("count VALUE (success): " + count);
+								  console.log("ERROR: ");
+								  console.log(error);
+								  count++;
+								  
+								  if ((count > tot) && (!resolved))
+								  {
+									  promise.resolve("Completed promise- called from error");
+									  resolved = true;
+								  }
+							  }
+						  });
+					  }//end for
+	
+			      }
+			    });
+   
+};
+
+exports.wantsnew = function(request, response) {
+  
+			var Want = Parse.Object.extend("Want");
+			var query = new Parse.Query(Want);
+			//query.equalTo("myuser", currentUser );
+			
+			query.find({
+			      success: function(usersWants) {
+			          // usersWants contains all of the wants by the current user.
+					  //console.log(userWants);
+					  
+					  var retUsers = [];
+					  var promise = new Parse.Promise();
+					  
+					  var resolved = false;
+					  
+					  promise.then(
+						  function(){
+							  console.log("EXECUTED PROMISE: ");
+						  	  response.success(retUsers);
+						  }
+					  );
+					  
+					  var tot = usersWants.length;
+					  var count = 1;
+					  
+					  for (var i = 0; i < usersWants.length; i++) { 
+						  var object = usersWants[i];
+						  var myjson = object.toJSON();
+						  retUsers.push(myjson);
+					  }
+					  
+					  for (var i = 0; i < usersWants.length; i++) { 
+						  
+						  console.log('PROCESSING: ' + i);
+						  console.log(object);
+						  
+						  var Provide = Parse.Object.extend("Provide");
+
+			  			  var q2 = new Parse.Query(Provide);
+						  q2.equalTo("city", retUsers[i].city );
+						  q2.equalTo("category", retUsers[i].category );
+						  q2.find({
+							  success: function(matchingProvides) {
+								  
+								  console.log("count VALUE (success): " + count);
+								  
+								  var cur =  retUsers[count-1];
+								  console.log("RetUsers:");
+								  console.log(retUsers);
+								  
+								  // usersWants contains all of the wants by the current user.
+								  //console.log(userWants);
+								  console.log('Found ' + matchingProvides.length + ' matches for want ' + retUsers[count-1].category);
+								  retUsers[count-1].matches = matchingProvides.length;
+								  
+								  count++;
+								  if ((count > tot) && (!resolved))
+								  {
+									  promise.resolve("Completed promise- called from success");
+									  resolved = true;
+								  }
+							  },
+							  error: function(error){
+								  
+								  retUsers[count-1].matches = 0;
+								  console.log("count VALUE (success): " + count);
+								  console.log("ERROR: ");
+								  console.log(error);
+								  count++;
+								  
+								  if ((count > tot) && (!resolved))
+								  {
+									  promise.resolve("Completed promise- called from error");
+									  resolved = true;
+								  }
+							  }
+						  });
+					  }//end for
+	
+			      }
+			    });
+   
+};
+
 
 exports.wants = function(request, response) {
   
@@ -92,11 +266,81 @@ exports.wants = function(request, response) {
 			var Want = Parse.Object.extend("Want");
 			var query = new Parse.Query(Want);
 			query.equalTo("myuser", currentUser );
+			
 			query.find({
 			      success: function(usersWants) {
-			        // usersWants contains all of the wants by the current user.
+			          // usersWants contains all of the wants by the current user.
 					  //console.log(userWants);
-					  response.success(usersWants);
+					  
+					  var retUsers = [];
+					  var promise = new Parse.Promise();
+					  
+					  var resolved = false;
+					  
+					  promise.then(
+						  function(){
+							  console.log("EXECUTED PROMISE: ");
+						  	  response.success(retUsers);
+						  }
+					  );
+					  
+					  var tot = usersWants.length;
+					  var count = 1;
+					  
+					  for (var i = 0; i < usersWants.length; i++) { 
+						  var object = usersWants[i];
+						  var myjson = object.toJSON();
+						  retUsers.push(myjson);
+					  }
+					  
+					  for (var i = 0; i < usersWants.length; i++) { 
+						  
+						  console.log('PROCESSING: ' + i);
+						  console.log(object);
+						  
+						  var Provide = Parse.Object.extend("Provide");
+
+			  			  var q2 = new Parse.Query(Provide);
+						  q2.equalTo("city", retUsers[i].city );
+						  q2.equalTo("category", retUsers[i].category );
+						  q2.find({
+							  success: function(matchingProvides) {
+								  
+								  console.log("count VALUE (success): " + count);
+								  
+								  var cur =  retUsers[count-1];
+								  console.log("RetUsers:");
+								  console.log(retUsers);
+								  
+								  // usersWants contains all of the wants by the current user.
+								  //console.log(userWants);
+								  console.log('Found ' + matchingProvides.length + ' matches for want ' + retUsers[count-1].category);
+								  retUsers[count-1].matches = matchingProvides.length;
+								  
+								  count++;
+								  if ((count > tot) && (!resolved))
+								  {
+									  promise.resolve("Completed promise- called from success");
+									  resolved = true;
+								  }
+							  },
+							  error: function(error){
+								  
+								  retUsers[count-1].matches = 0;
+								  console.log("count VALUE (success): " + count);
+								  console.log("ERROR: ");
+								  console.log(error);
+								  count++;
+								  
+								  if ((count > tot) && (!resolved))
+								  {
+									  promise.resolve("Completed promise- called from error");
+									  resolved = true;
+								  }
+							  }
+						  });
+					  }//end for
+	
 			      }
 			    });
 			
