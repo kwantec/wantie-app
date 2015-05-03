6b921bee59fc6b7fe267711ad4492765d61c08ac
Daniel Castaneda <daniel@kwantec.com>
2015-05-03 07:49:33 -0400

    Added web site changes

--- backend/Wantie/public/dist/index.js
+++ backend/Wantie/public/dist/index.js
@@ -1,8 +1,68 @@
 var app = angular.module('myApp', ['ui.bootstrap']);
 app.controller('tabsController', function($scope, $window, $http) {
 
+	$scope.provides = [
+		{
+			category: 'Dentist',
+			city: 'Miami',
+			matches: 1
+		},		
+		{
+			category: 'Trainer',
+			city: 'Miami',
+			matches: 0
+		},		
+		{
+			category: 'Driver',
+			city: 'Miami',
+			matches: 1
+		}
+	];
 
+	$scope.wants = [
+		{
+			category: 'Mechanic',
+			city: 'Miami',
+			matches: 2
+		},		
+		{
+			category: 'Chef',
+			city: 'Miami',
+			matches: 0
+		}
+		];
 
-	
+	$scope.settings = {
+		screenName: '',
+		phone: '',
+		email: '',
+		city: 'Miami'
+	};
+
+	$scope.isloggedIn = false;
+
+	$scope.selectedProvide = function()
+	{
+		console.log('Selected provide');
+	};
+
+	$scope.selectedNeed = function()
+	{
+		console.log('Selected need');
+	};
+
+	$scope.selectedSettings = function()
+	{
+		console.log('Selected settings');
+	};
+
+	$scope.loggedOut = function()
+	{
+		$scope.isloggedIn = false;
+	};
+	$scope.loggedIn = function()
+	{
+		$scope.isloggedIn = true;
+	};
 	
 });	
